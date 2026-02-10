7db42aeb34199bc538f25ac0fcf44a70935ca50e
okarun4k-sudo <okarun4k@gmail.com>
2026-02-10 09:52:01 -0300

    Create _data.js

---
+++ api/_data.js
@@ -0,0 +1,33 @@
+// api/_data.js
+export const mangas = [
+  {
+    id: 1,
+    title: "Jukai",
+    description: "O Templo Kuyou é um lugar...",
+    coverUrl: "https://i.postimg.cc/s2sHRG3V/fdcad01e-1c7d-4ae0-bcd1-2e3e7465a7a0-jpg-512.jpg",
+    chapters: [
+      {
+        chapterNumber: 1,
+        title: "O Monstro Gigante da Montanha",
+        pages: [
+          "https://files.catbox.moe/evulnx.png",
+          "https://files.catbox.moe/f7my39.png"
+          // ... adicione as outras URLs aqui
+        ]
+      }
+    ]
+  },
+  {
+    id: 17,
+    title: "HP5 DESU KEDO JIDOU CHIYU",
+    description: "Aimi Asoyama...",
+    coverUrl: "https://files.catbox.moe/r6udjq.jpg",
+    chapters: [
+      {
+        chapterNumber: 1,
+        title: "Capítulo 1",
+        pages: ["https://files.catbox.moe/sobhi3.png"]
+      }
+    ]
+  }
+];
