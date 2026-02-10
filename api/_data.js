df9ed9d9f7fb1ab1bbcb4789cffe5283f080c6d4
okarun4k-sudo <okarun4k@gmail.com>
2026-02-10 09:53:31 -0300

    Add new manga entries to _data.js
    Added new manga entries with details including chapters and pages.

--- api/_data.js
+++ api/_data.js
@@ -1,33 +1,59 @@
 // api/_data.js
 export const mangas = [
-  {
-    id: 1,
-    title: "Jukai",
-    description: "O Templo Kuyou é um lugar...",
-    coverUrl: "https://i.postimg.cc/s2sHRG3V/fdcad01e-1c7d-4ae0-bcd1-2e3e7465a7a0-jpg-512.jpg",
-    chapters: [
-      {
-        chapterNumber: 1,
-        title: "O Monstro Gigante da Montanha",
-        pages: [
-          "https://files.catbox.moe/evulnx.png",
-          "https://files.catbox.moe/f7my39.png"
-          // ... adicione as outras URLs aqui
+    {
+        id: 19,
+        title: "Youjo Robot to Hentai Hakase",
+        description: "Nova webcomic da criadora de Happy Happia. A história acompanha uma professora pervertida que se apaixona por sua criação.",
+        author: "Ino (いの)",
+        genres: ["Comédia", "Amor entre garotas", "Um recorte da vida"],
+        translationTeam: "MangaTachi",
+        status: "Em andamento",
+        coverUrl: "https://8upload.com/image/8a0203c4c4095dc2/caps.jpg",
+        chapters: [
+            {
+                chapterNumber: 1,
+                title: "Capítulo 1",
+                pages: ["https://8upload.com/image/06e9f477bf4a06c1/01.png", "https://8upload.com/image/9bcfe2774e662583/02.png"]
+            },
+            {
+                chapterNumber: 2,
+                title: "Capítulo 2",
+                pages: ["https://8upload.com/image/f73f0664ccfcbae1/pagg1.png", "https://8upload.com/image/6b562be5bcbebab2/pagg2.png"]
+            }
         ]
-      }
-    ]
-  },
-  {
-    id: 17,
-    title: "HP5 DESU KEDO JIDOU CHIYU",
-    description: "Aimi Asoyama...",
-    coverUrl: "https://files.catbox.moe/r6udjq.jpg",
-    chapters: [
-      {
-        chapterNumber: 1,
-        title: "Capítulo 1",
-        pages: ["https://files.catbox.moe/sobhi3.png"]
-      }
-    ]
-  }
+    },
+    {
+        id: 17,
+        title: "HP5 DESU KEDO JIDOU CHIYU",
+        description: "Aimi Asoyama, uma estudante do primeiro ano... adquire a habilidade única Cura Automática.",
+        author: "Yuto Yotsuba",
+        genres: ["Action", "Adventure", "Comedy", "Fantasy", "School Life"],
+        translationTeam: "Manga Tachi",
+        status: "Ongoing",
+        coverUrl: "https://files.catbox.moe/r6udjq.jpg",
+        chapters: [
+            {
+                chapterNumber: 1,
+                title: "Capítulo 1",
+                pages: ["https://files.catbox.moe/sobhi3.png", "https://files.catbox.moe/rjbc7s.png", "https://files.catbox.moe/wyliug.png"]
+            }
+        ]
+    },
+    {
+        id: 1,
+        title: "Jukai",
+        description: "O Templo Kuyou é um lugar onde são levados todos os tipos de objetos amaldiçoados...",
+        author: "Saito Ryoga",
+        genres: ["Horror", "Mistério", "Psicológico", "Tragédia"],
+        translationTeam: "MangaRoom",
+        status: "Em andamento",
+        coverUrl: "https://i.postimg.cc/s2sHRG3V/fdcad01e-1c7d-4ae0-bcd1-2e3e7465a7a0-jpg-512.jpg",
+        chapters: [
+            {
+                chapterNumber: 1,
+                title: "O Monstro Gigante da Montanha",
+                pages: ["https://files.catbox.moe/evulnx.png", "https://files.catbox.moe/f7my39.png", "https://files.catbox.moe/zxad4q.png"]
+            }
+        ]
+    }
 ];
